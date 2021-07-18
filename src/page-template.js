5a8ac3c877af6cf05a7d8ee43edc62782ec69c6d
Javier Dominguez II <Javi7057@gmail.com>
2021-07-17 20:26:32 -0500

    initial html, almost done, then putting into page-template.js

---
+++ src/page-template.js
@@ -0,0 +1,28 @@
+module.exports = templateData => {
+    console.log(templateData);
+
+    // destructure projects and about data from templateData based on their property key names
+    // ... stands for 'rest operator' where it's used, often confused with the 'spread operator' in this context
+
+    // const {projects, about, ... header } = templateData;
+
+    // console.log(projects);
+    // console.log(about);
+    // console.log(header);
+
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile Generator</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        </head>
+    <body>
+        
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </body>
+    </html>`;
+};
